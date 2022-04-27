e44d0bc129c41cf97a1d1b8960148f5025efe04d
SulthanRaghib <raghib.smp@gmail.com>
2022-04-27 21:25:55 +0700

    change line 4 and 22

--- praktikum05/1.2.Dispenser/data.dispenser.php
+++ praktikum05/1.2.Dispenser/data.dispenser.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'class.dispenser.php';
 
-$penjual = new Dispenser("Big Cola", "");
+$penjual = new Dispenser("Big Cola");
 echo '<b>Mesin penjual : ' . $penjual->nama_muniman;
 echo '<br>Memiliki kapasitas Volume Galon sebesar : ' . number_format($penjual->getVolume()) . 'ml</b>';
 echo "<br>";
@@ -19,4 +19,4 @@ $pembeli3 = new Pembeli('Raghib', 4, 'Big Cola');
 echo $pembeli3->getStrukPenjualan();
 echo "<br>";
 echo "<br>";
-echo Pembeli::tambahVolume(1000);
+echo Dispenser::tambahVolume(1000);
