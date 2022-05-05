1dc143a27d583fc80a38dfd41347107d2041d452
SulthanRaghib <raghib.smp@gmail.com>
2022-05-05 23:37:27 +0700

    config CodeIgniter

--- praktikum06/Web-Framework/Rafii-Yuuki/application/config/config.php
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/Web-Pro-II-SulthanRaghibFillah-TI01/praktikum06/Web-Framework/Rafii-Yuuki/';
 
 /*
 |--------------------------------------------------------------------------
