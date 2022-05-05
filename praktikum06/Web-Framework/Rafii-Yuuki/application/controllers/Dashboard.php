ff0bac3f1d59b6180bafa99cddd0386f8844cab0
SulthanRaghib <raghib.smp@gmail.com>
2022-05-06 00:27:21 +0700

    add template AdminLTE

--- praktikum06/Web-Framework/Rafii-Yuuki/application/controllers/Dashboard.php
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/controllers/Dashboard.php
@@ -5,6 +5,11 @@ class Dashboard extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('layout/template');
+        $judul['judul'] = 'Dashboard';
+
+        $this->load->view('layout/header', $judul);
+        $this->load->view('layout/sidebar');
+        $this->load->view('dashboard/index');
+        $this->load->view('layout/footer');
     }
 }
---
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/views/layout/footer.php
@@ -0,0 +1,26 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?= base_url('public/plugins/jquery/jquery.min.js') ?>"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('public/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('public/dist/js/adminlte.min.js') ?>"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= base_url('public/dist/js/demo.js') ?>"></script>
+</body>
+
+</html>
