143ae26b5d64442aa3b39118b7ece44ea825118c
SulthanRaghib <raghib.smp@gmail.com>
2022-05-07 08:05:58 +0700

    update model

--- praktikum06/Web-Framework/Rafii-Yuuki/application/models/Dosen_model.php
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/models/Dosen_model.php
@@ -2,11 +2,10 @@
 class Dosen_model extends CI_Model
 {
     public $id;
-    public $nama;
     public $nidn;
+    public $nama;
     public $jenis_kelamin;
     public $tmp_lahir;
     public $tgl_lahir;
-    public $kelas;
     public $pendidikan_terakhir;
 }
--- praktikum06/Web-Framework/Rafii-Yuuki/application/models/Matakuliah_model.php
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/models/Matakuliah_model.php
@@ -5,4 +5,5 @@ class Matakuliah_model extends CI_Model
     public $matkul;
     public $sks;
     public $kode;
+    public $dosen_pengajar;
 }
