c3353dafb383c7c42d151785a60baa762712a4cb
SulthanRaghib <raghib.smp@gmail.com>
2022-05-24 20:13:16 +0700

    delete database

--- praktikum06/Web-Framework/Rafii-Yuuki/application/config/database.php
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
+	'hostname' => '',
+	'username' => '',
 	'password' => '',
-	'database' => 'db_crud_masis',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
