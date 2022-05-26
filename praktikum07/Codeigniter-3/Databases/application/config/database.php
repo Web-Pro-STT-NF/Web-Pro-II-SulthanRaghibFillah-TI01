8910ad9bba50e61dda9ae36c0fe9b7d53d205c53
SulthanRaghib <raghib.smp@gmail.com>
2022-05-26 22:21:22 +0700

    setup database and connect to database dbkampus

--- praktikum07/Codeigniter-3/Databases/application/config/database.php
+++ praktikum07/Codeigniter-3/Databases/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
+	'hostname' => 'localhost',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'dbkampus',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
