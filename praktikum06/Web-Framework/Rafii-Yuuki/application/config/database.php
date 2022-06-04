cdec786afd2b78bd6e3727ea8a1a442e6ca8bc6e
SulthanRaghib <raghib.smp@gmail.com>
2022-06-04 20:50:43 +0700

    delete database access

--- praktikum06/Web-Framework/Rafii-Yuuki/application/config/database.php
+++ praktikum06/Web-Framework/Rafii-Yuuki/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
+	'hostname' => '',
+	'username' => '',
 	'password' => '',
-	'database' => 'dbkampus',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
