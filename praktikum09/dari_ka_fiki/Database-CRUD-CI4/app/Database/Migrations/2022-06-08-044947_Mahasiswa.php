2a47e02d8eaad8af4dc3b3a59ba4b95192d1d45c
SulthanRaghib <raghib.smp@gmail.com>
2022-06-08 12:40:10 +0700

    crete table mahasiswa

--- praktikum09/dari_ka_fiki/Database-CRUD-CI4/app/Database/Migrations/2022-06-08-044947_Mahasiswa.php
+++ praktikum09/dari_ka_fiki/Database-CRUD-CI4/app/Database/Migrations/2022-06-08-044947_Mahasiswa.php
@@ -8,7 +8,54 @@ class Mahasiswa extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'                    => [
+                'type'              => 'INT', // type id biasanya INTEGER
+                'constraint'        => 11, // constraint adalah besar penyimpanan ata membatasi
+                'unsigned'          => true, // unsigned adalah bilangan bulat yang tidak pernah bisa negatif.
+                'auto_increment'    => true
+            ],
+            'nim'                   => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '30' // constraint milik varchar wajib pakai ''
+            ],
+            'nama'                  => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '50'
+            ],
+            'jenis_kelamin'         => [
+                'type'              => 'ENUM', // enum adalah tipe data yg punya pilihan atau tipe data khusus yg memungkinkan variabel menjadi satu set konstanta yg telah ditentukan sebelumnya.
+                'constraint'        => "'Laki-Laki', 'Perempuan'"
+            ],
+            'tmp_lahir'             => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '20'
+            ],
+            'tgl_lahir'             => [
+                'type'              => 'DATETIME'
+            ],
+            'program_study'         => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '30'
+            ],
+            'ipk'                   => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '5'
+            ],
+            'creaeted_at'           => [ //diisi oleh system
+                'type'              => 'TIMESTAMP',
+                'null'              => true,
+                'on update'         => 'NOW()'
+            ],
+            'update_at'             => [ //diisi oleh system
+                'type'              => 'TIMESTAMP',
+                'null'              => true,
+                'on update'         => 'NOW()'
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('mahasiswa');
     }
 
     public function down()
